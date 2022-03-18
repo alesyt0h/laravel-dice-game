4bdd95480d3146a9a14d4138e7757d210c3b4365
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-18 20:52:33 +0100

    Added all the needed routes

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/players')->group( function(){
+
+    Route::post('/', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+    Route::middleware(['auth:api'])->group(function () {
+        Route::get('/', [UserController::class, 'getUsers'])->name('players');
+        Route::put('/{id}', [UserController::class, 'update'])->name('update.player');
+        Route::delete('/{id}/games', [UserController::class, 'deleteThrows'])->name('delete.throws');
+        Route::post('/{id}/games', [GameController::class, 'play'])->name('play');
+        Route::get('/{id}/games', [GameController::class, 'getThrows'])->name('all.throws');
+        Route::get('/ranking', [GameController::class, 'ranking'])->name('ranking');
+        Route::get('/ranking/loser', [GameController::class, 'loser'])->name('loser');
+        Route::get('/ranking/winner', [GameController::class, 'winner'])->name('winner');
+    });
 });
