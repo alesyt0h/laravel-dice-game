2769c59f08f6c667cd5f44a8be712f3c1adf656c
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-18 22:37:04 +0100

    Added the Register method in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -13,5 +18,36 @@ class AuthController extends Controller
 
     public function register(Request $request){
 
+        $credentials = Validator::make($request->all(), [
+            'nickname' => ['string', 'min:4','max:15'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', Rules\Password::defaults()],
+        ]);
+
+        if ($credentials->fails()){
+            return response()->json(['errors' => $credentials->messages()], Response::HTTP_BAD_REQUEST);
+        }
+
+        // TODO Make Validation Rule
+        $match = User::select('nickname')->where('nickname', $request->nickname)->get();
+
+        if(count($match)){
+            return response(['message' => 'This nickname is already taken. Please choose another.']);
+        }
+        //
+
+        if(! ($request->nickname ?? '')){
+            $request->nickname = 'Anonymous';
+        }
+
+        $user = User::create([
+            'nickname' => $request->nickname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $accessToken  = $user->createToken('gameAccessToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $accessToken]);
     }
 }
