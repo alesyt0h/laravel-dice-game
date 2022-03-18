162dcc321608d6ace2e2ff96c61b5d8939ee83d0
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-18 23:39:26 +0100

    Created Play method for throwing the dices

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     public function play(Int $id){
+
+        if(!isSameUser($id)) return response(['message' => 'Unauthorized']);
+
+        $blackDice = rand(1, 6);
+        $redDice = rand(1, 6);
+
+        $result = ($blackDice + $redDice === 7) ? 'lost' : 'win';
+
+        $throw = Game::create([
+            'player_id' => $id,
+            'black_dice' => $blackDice,
+            'red_dice' => $redDice,
+            'result' => $result
+        ]);
+
+        unset($throw->player_id);
+
+        return response(['result' => $throw]);
 
     }
 
