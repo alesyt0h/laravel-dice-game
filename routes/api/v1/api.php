e30c492420092ac1bdd8333645f045d596e6d1a3
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-19 18:14:29 +0100

    Protected the Get All Users and Ranking Routes for only admin

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -23,12 +23,12 @@ Route::prefix('/players')->group( function(){
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 
     Route::middleware(['auth:api'])->group(function () {
-        Route::get('/', [UserController::class, 'getUsers'])->name('players');
+        Route::get('/', [UserController::class, 'getUsers'])->name('players')->middleware('admin');
         Route::put('/{id}', [UserController::class, 'update'])->name('update.player');
         Route::delete('/{id}/games', [UserController::class, 'deleteThrows'])->name('delete.throws');
         Route::post('/{id}/games', [GameController::class, 'play'])->name('play');
         Route::get('/{id}/games', [GameController::class, 'getThrows'])->name('all.throws');
-        Route::get('/ranking', [GameController::class, 'ranking'])->name('ranking');
+        Route::get('/ranking', [GameController::class, 'ranking'])->name('ranking')->middleware('admin');
         Route::get('/ranking/loser', [GameController::class, 'loser'])->name('loser');
         Route::get('/ranking/winner', [GameController::class, 'winner'])->name('winner');
     });
