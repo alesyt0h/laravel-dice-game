682f200f8b1be816e66068009264d53890ae9210
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-19 12:33:04 +0100

    Added the method: Get All Players with their winning percentage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -16,6 +17,10 @@ class UserController extends Controller
 
     public function getUsers(){
 
+        $users = User::all();
+        perUserPercentage($users);
+
+        return $users;
     }
 
 
