abbb2dedf503dacac930b89c4f8c06482ae1bea9
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-21 19:35:55 +0100

    Get All Players Route Tests - Forbidden and Successful

--- tests/Feature/CRUDTest.php
+++ tests/Feature/CRUDTest.php
@@ -25,4 +25,38 @@ class CRUDTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /** @test */
+    public function get_all_players_returns_200_and_users()
+    {
+        $user = User::factory()->make();
+        $user = Passport::actingAs($user, ['administrate']);
+
+        User::factory(10)->create();
+
+        $response = $this->get(route('players'), ['Accept' => 'application/json']);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            [
+                'id',
+                'nickname',
+                'email',
+                'is_admin',
+                'created_at',
+                'updated_at',
+                'winning_percentage'
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function get_all_players_returns_403_if_user_is_not_admin()
+    {
+        $user = User::factory()->make();
+        $user = Passport::actingAs($user);
+
+        User::factory(10)->create();
+
+        $response = $this->get(route('players'), ['Accept' => 'application/json']);
+        $response->assertStatus(403);
+    }
 }
