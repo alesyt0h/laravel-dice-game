8a7d5618f2a40ab55089a249240c87f215730586
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-21 21:12:40 +0100

    Test Ranking loser and winner modified

--- tests/Feature/CRUDTest.php
+++ tests/Feature/CRUDTest.php
@@ -13,33 +13,21 @@ class CRUDTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function ranking_loser_and_winner_returns_200_status()
-    {
-        $user = User::factory()->make();
-        $user = Passport::actingAs($user);
-
-        $response = $this->get(route('loser'), ['Accept' => 'application/json']);
-        $response->assertStatus(200);
-
-        $response = $this->get(route('winner'), ['Accept' => 'application/json']);
-        $response->assertStatus(200);
-    }
-
-    /** @test */
     public function ranking_loser_and_winner_returns_200_and_expected_user_JSON()
     {
         $admin = User::factory()->make();
         $admin = Passport::actingAs($admin, ['administrate']);
 
-        $users = User::factory(10)->create();
+        User::factory(10)->create();
 
-        foreach ($users as $user) {
-            $response = $this->post(route('play', $user->id), ['Accept' => 'application/json']);
+        for ($i=0; $i < 10; $i++) {
+            Game::factory(1)->create([
+                'id' => $i + 1,
+                'player_id' => $i + 1
+            ]);
         }
 
         $response = $this->get(route('loser'), ['Accept' => 'application/json']);
-
-        // var_dump($response->json());
 
         $response->assertStatus(200);
         $response->assertJsonStructure([
