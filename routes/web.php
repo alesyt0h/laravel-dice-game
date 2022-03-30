40c319a9d5ea23b805b6f6248a73496d8514bbb9
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-30 15:17:02 +0200

    Route Fix - Gitignore Fix for PassportKeys & Auto Deployment Passport Keys

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
+Route::get('/{any}', function () {
     return File::get(public_path() . '/index.html');
-});
+})->where('any', '(.*)');
